6aa24d116efeec02aa52dfa68dcd4bc369c5c583
Nina Bornemann <nina.bornemann@freenet.de>
2025-09-17 01:20:02 -0700

    Initial commit

---
+++ src/main/java/com/ninabornemann/todo_backend_project/ToDoBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ninabornemann.todo_backend_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoBackendProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoBackendProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ninabornemann/todo_backend_project/ToDoBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninabornemann.todo_backend_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoBackendProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
