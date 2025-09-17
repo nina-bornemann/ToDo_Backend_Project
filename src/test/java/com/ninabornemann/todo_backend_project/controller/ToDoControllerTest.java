4c36c1c7ea2c86a328581bdc1da514b4e32da0ff
Nina Bornemann <nina.bornemann@freenet.de>
2025-09-17 07:37:31 -0700

    add Integration tests for Todo Controller

--- src/test/java/com/ninabornemann/todo_backend_project/controller/ToDoControllerTest.java
+++ src/test/java/com/ninabornemann/todo_backend_project/controller/ToDoControllerTest.java
@@ -1,5 +1,8 @@
 package com.ninabornemann.todo_backend_project.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ninabornemann.todo_backend_project.dto.ToDoDto;
 import com.ninabornemann.todo_backend_project.models.Status;
 import com.ninabornemann.todo_backend_project.models.Todo;
 import com.ninabornemann.todo_backend_project.repository.TodoRepo;
@@ -8,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -28,6 +33,7 @@ class ToDoControllerTest {
     @Autowired
     private TodoRepo repo;
 
+    @DirtiesContext
     @Test
     void getAllToDos() throws Exception {
         Todo t1 = new Todo("123", "start Spring", Status.OPEN);
@@ -37,15 +43,89 @@ class ToDoControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json(
                         """
-                                       [
-                                           {
-                                             "id": "123",
-                                             "description": "start Spring",
-                                             "status": "OPEN"
-                                           }
-                                       ]
-                                   """
+                                    [
+                                        {
+                                          "id": "123",
+                                          "description": "start Spring",
+                                          "status": "OPEN"
+                                        }
+                                    ]
+                                """
                 ));
+    }
+
+    @DirtiesContext
+    @Test
+    void addTodo() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/todo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "description": "test Spring",
+                                    "status": "OPEN"
+                                }
+                                """))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                        {
+                            "description": "test Spring",
+                            "status": "OPEN"
+                        }
+                        """))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNotEmpty());
+    }
+
+    @DirtiesContext
+    @Test
+    void getTodoById() throws Exception {
+        Todo t1 = new Todo("123", "start Spring", Status.OPEN);
+        repo.save(t1);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/todo/123"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                        {
+                          "id": "123",
+                          "description": "start Spring",
+                          "status": "OPEN"
+                        }
+                        """));
+    }
+
+    @DirtiesContext
+    @Test
+    void updateToDoById() throws Exception {
+        Todo t1 = new Todo("123", "start Spring", Status.OPEN);
+        repo.save(t1);
+        ToDoDto dto = new ToDoDto("is still working?", Status.DONE);
+
+        String json = new ObjectMapper().writeValueAsString(dto);
+        System.out.println(json);
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/api/todo/123")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                        {
+                            "id": "123",
+                            "description": "is still working?",
+                            "status": "DONE"
+                        }
+                        """));
+    }
+
+    @DirtiesContext
+    @Test
+    void deleteById() throws Exception {
+        Todo t1 = new Todo("123", "start Spring", Status.OPEN);
+        repo.save(t1);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/todo/123"))
+                .andExpect(MockMvcResultMatchers.status().isNoContent())
+                .andExpect(MockMvcResultMatchers.content().string(""))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").doesNotExist());
     }
 
 }
